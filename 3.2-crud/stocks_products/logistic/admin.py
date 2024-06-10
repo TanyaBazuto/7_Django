a5d9e7e0fcd0db59b15289ba0ed2c243358cf7b3
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-10 14:34:41 +0300

    Update admin.py

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product, Stock, StockProduct
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['id', 'address']
+
+
+@admin.register(StockProduct)
+class StockProductAdmin(admin.ModelAdmin):
+    list_display = ['stock', 'product', 'quantity', 'price']
+    
