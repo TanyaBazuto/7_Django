a78430852660b963b30168b7599091e7f5e466aa
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-10 14:38:26 +0300

    Update settings.py

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
+        'NAME': 'stocks_products_db',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
     }
 }
 
