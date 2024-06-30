f4230bd39d9a42fba881ecd0e4fd85e3f24ac5b3
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-30 23:50:01 +0300

    Update serializers.py

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
 from advertisements.models import Advertisement
 
 
@@ -23,7 +22,7 @@ class AdvertisementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advertisement
         fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at', )
+                  'status', 'created_at', 'updated_at')
 
     def create(self, validated_data):
         """Метод для создания"""
@@ -34,12 +33,20 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         # обратите внимание на `context` – он выставляется автоматически
         # через методы ViewSet.
         # само поле при этом объявляется как `read_only=True`
-        validated_data["creator"] = self.context["request"].user
+        validated_data['creator'] = self.context['request'].user
         return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['creator'] = self.context['request'].user
+        return super().update(instance, validated_data)
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
         # TODO: добавьте требуемую валидацию
+        count_advertisements = Advertisement.objects.filter(status='OPEN', creator=self.context['request'].user).count()
+        status = data.get('status')
+        if count_advertisements >= 10 and self.context['request'].method == 'POST' or status == 'OPEN':
+            raise serializers.ValidationError('Доступно не более 10 объявлений')
 
         return data
