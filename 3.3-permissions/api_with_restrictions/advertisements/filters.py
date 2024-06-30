205c93ab1502eb77ab422da06ab1563824364c95
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-30 23:48:07 +0300

    Update filters.py

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -7,6 +7,9 @@ class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
     # TODO: задайте требуемые фильтры
-
+    status = filters.CharFilter()
+    created_at = filters.DateFromToRangeFilter()
     class Meta:
         model = Advertisement
+        fields = ['status', 'created_at']
+        
